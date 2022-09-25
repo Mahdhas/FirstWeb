a4eafa3a3c63a9b35332620bb410ab2a10d86832
mhassouna <mhassouna@GTC-MHASSOUNA-D>
2022-09-25 10:28:45 +0300

    Merge branch 'v2' of https://github.com/Mahdhas/FirstWeb into v2Add project files.

---
+++ FirstWeb/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using FirstWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWeb.Controllers
+{
+    //[Route("/[action]")]
+    //[Route("/")]
+    public class HomeController : Controller
+    {
+        //[Route("[action]/page/{cat?}")]        
+        GuestList g = new GuestList();
+        public IActionResult Index(string? cat="12")
+        {
+            ViewBag.cat = cat;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Guest()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Guest(Guest ReCGuest)
+        {
+            g.AddGuest(ReCGuest);
+            return View("Thank", ReCGuest);
+        }
+
+        [Route("All")]
+        public IActionResult AllGuests()
+        {
+          
+            return View(g.AllGuests());
+        }
+        [Route("[action]")]
+        public ContentResult Test(int? Id)
+        {
+          
+            return Content("<html> <h1> Welcome </h1> </html>");
+        }
+    }
+}
